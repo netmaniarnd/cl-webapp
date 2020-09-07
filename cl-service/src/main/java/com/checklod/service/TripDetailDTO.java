4f26e81cad034b9458963c22007cac8b917b8b57
jangmj <jangmj@hanafos.com>
2020-09-07 18:00:37 +0900

    Revert "detailDTO prework"
    This reverts commit 4d08cb2ce4853b8e119f1934adbe661c38745e4f.

--- cl-service/src/main/java/com/checklod/service/TripDetailDTO.java
+++ cl-service/src/main/java/com/checklod/service/TripDetailDTO.java
@@ -1,7 +1,5 @@
 package com.checklod.service;
 
-import java.util.List;
-
 import lombok.Data;
 
 @Data
@@ -9,12 +7,4 @@ public class TripDetailDTO {
 
 	private String tripId;
 	private String invoiceId;
-	private ReportSummary reportSummary;
-	private DeviceInfo devieInfo;
-	private String driverName;
-	private String phoneNo;
-	private String vehicleNo;
-	private String loggerAlias;
-	private List<PointTemp> listTemp;
-	private List<MediaInfo> listMedia;
 }
