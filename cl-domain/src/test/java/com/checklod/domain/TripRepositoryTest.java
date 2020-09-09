914294fb1b688c56d044435d559d3676f016b9c1
jangmj <jangmj@hanafos.com>
2020-09-09 13:52:37 +0900

    domain layer test error fixed

--- cl-domain/src/test/java/com/checklod/domain/TripRepositoryTest.java
+++ cl-domain/src/test/java/com/checklod/domain/TripRepositoryTest.java
@@ -7,13 +7,22 @@ import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.context.annotation.Import;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase.Replace;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
 
 @ExtendWith(SpringExtension.class)
-@SpringBootTest
-@Import(com.checklod.domain.TripRepository.class)
+@AutoConfigureTestDatabase(replace = Replace.NONE)
+@TestPropertySource(locations="classpath:application.properties")
+@DataJpaTest
+@ContextConfiguration(classes = {com.checklod.domain.TripRepository.class, com.checklod.domain.LoggerRepository.class, com.checklod.domain.TripMediaRepository.class})
+@EnableJpaRepositories(basePackages = {"com.checklod.domain"})
+@EntityScan("com.checklod.domain")
 class TripRepositoryTest {
 
 	@Autowired
